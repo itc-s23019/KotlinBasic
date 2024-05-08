d7e1d870e87acda4aa2fe4a3f9d496d8292fb58b
s23019 <s23019@std.it-college.ac.jp>
2024-05-08 17:01:54 +0900

    6章終わり

---
+++ src/main/kotlin/Chap06.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val num = 100
+    val a : Boolean = (num == 100)
+    val b : Boolean = (num == 125)
+    println("変数 a の値は、 $a です。")
+    println("変数 a の値は、 $b です。")
+}

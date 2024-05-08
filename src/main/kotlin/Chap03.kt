6d0877cdcbad4137f25d5eb785407040c6d7c7f3
s23019 <s23019@std.it-college.ac.jp>
2024-05-08 17:01:28 +0900

    3章終わり

---
+++ src/main/kotlin/Chap03.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main(args: Array<String>) {
+    //val message : String = "Hello, world!"
+    val message = "こんにちは、世界!"
+    println(message)
+    println(message)
+    println(message)
+}
